b90698fffeb9b02c2d7bb1016acc5465ba341638
Fabricio <earthbornshepard@hotmail.com>
2020-06-09 11:54:19 -0300

    mudancas no package

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Home from "./Home";
 
 export class AppContainer extends Component {
   render() {
-    return (
-      <div>
-
-      </div>
-    )
+    return <div></div>;
   }
 }

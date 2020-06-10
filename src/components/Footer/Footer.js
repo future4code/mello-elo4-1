daa67f0dc4ef1a536d67bad79fbbecefdad4810b
Fabricio <earthbornshepard@hotmail.com>
2020-06-10 10:40:47 -0300

    Criação do footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,90 @@
 import React from "react";
-import { MainContainer } from "./styled";
+import {
+  MainContainer,
+  ListItemFooter,
+  SocialMediaIcon,
+  SocialMediaContainer,
+  ListFooter,
+} from "./styled";
 
-class Footer extends React.Component {
-  state = {};
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
+import Input from "@material-ui/core/Input";
+import PaymentMethodsPicture from "./paymentMethods.png";
 
-  render() {
-    return <MainContainer>Footer</MainContainer>;
-  }
+import Button from "@material-ui/core/Button";
+
+const teamName = "< Equipe DevSerImpecável />";
+
+const mainTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#584DA8",
+    },
+    secondary: {
+      main: "#99D19C",
+    },
+  },
+});
+
+function Footer(props) {
+  return (
+    <MuiThemeProvider theme={mainTheme}>
+      <MainContainer>
+        <ListFooter>
+          <h4>Redes Sociais</h4>
+          <SocialMediaContainer>
+            <ListItemFooter>
+              <SocialMediaIcon src="https://cdn3.iconfinder.com/data/icons/wpzoom-developer-icon-set/500/01-512.png" />
+            </ListItemFooter>
+            <ListItemFooter>
+              <SocialMediaIcon src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png" />
+            </ListItemFooter>
+            <ListItemFooter>
+              <SocialMediaIcon src="https://cdn3.iconfinder.com/data/icons/picons-social/57/43-twitter-512.png" />
+            </ListItemFooter>
+            <ListItemFooter>
+              <SocialMediaIcon src="https://cdn3.iconfinder.com/data/icons/picons-social/57/11-linkedin-512.png" />
+            </ListItemFooter>
+            <ListItemFooter>
+              <SocialMediaIcon src="https://cdn3.iconfinder.com/data/icons/picons-social/57/73-pinterest-512.png" />
+            </ListItemFooter>
+          </SocialMediaContainer>
+        </ListFooter>
+        <ListFooter>
+          <h4>Newsletter</h4>
+          <ListItemFooter>
+            Receba novidades personalizadas no seu e-mail
+          </ListItemFooter>
+          <ListItemFooter>
+            <Input type="email" placeholder="digite aqui seu email"></Input>
+            <Button
+              size="small"
+              variant="outlined"
+              color="primary"
+              onClick={() => alert("E-mail enviado com sucesso!")}
+            >
+              Assinar
+            </Button>
+          </ListItemFooter>
+        </ListFooter>
+        <ListFooter>
+          <h4>Métodos de pagamento</h4>
+          <img src={PaymentMethodsPicture} alt="" />
+        </ListFooter>
+        <ListFooter>
+          <h4>Deixe-nos ajudá-lo</h4>
+          <ListItemFooter>Sua conta</ListItemFooter>
+          <ListItemFooter>Devoluções e reembolsos</ListItemFooter>
+          <ListItemFooter>Ajuda</ListItemFooter>
+          <ListItemFooter>SAC</ListItemFooter>
+        </ListFooter>
+        <ListFooter>
+          <h4>Feito por</h4>
+          <ListItemFooter>{teamName}</ListItemFooter>
+        </ListFooter>
+      </MainContainer>
+    </MuiThemeProvider>
+  );
 }
 
 export default Footer;
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,5 +1,34 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-  border: 1px solid red;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  height: 200px;
+`;
+
+export const ListItemFooter = styled.li`
+  list-style: none;
+`;
+
+export const FooterInput = styled.input`
+  min-height: 4.5vh;
+  margin-right: 10px;
+`;
+
+export const SocialMediaIcon = styled.img`
+  max-width: 32px;
+  max-height: 32px;
+  cursor: pointer;
+`;
+
+export const SocialMediaContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 250px;
+`;
+
+export const ListFooter = styled.ul`
+  width: 100%;
 `;

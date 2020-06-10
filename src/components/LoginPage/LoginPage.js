a8eb9603041de6602452a644f3dc9dd4afa36a3a
Fabricio <earthbornshepard@hotmail.com>
2020-06-10 10:38:36 -0300

    página de login feita

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,12 +1,54 @@
 import React from "react";
-import { MainContainer } from "./styled";
 
-class LoginPage extends React.Component {
-  state = {};
+import { MainContainer, HeaderText, ButtonsContainer } from "./styled";
 
-  render() {
-    return <MainContainer>Login</MainContainer>;
-  }
+import Button from "@material-ui/core/Button";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
+
+import SupplierIcon from "@material-ui/icons/Group";
+import UserIcon from "@material-ui/icons/Person";
+
+const mainTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#584DA8",
+    },
+    secondary: {
+      main: "#99D19C",
+    },
+  },
+});
+
+function LoginPage(props) {
+  const { changePage } = props;
+
+  return (
+    <MuiThemeProvider theme={mainTheme}>
+      <MainContainer>
+        <HeaderText>Bem-vindo à Elo4</HeaderText>
+        <ButtonsContainer>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={() => changePage("productsGrid")}
+          >
+            <UserIcon />
+            Consumidor
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            onClick={() => changePage("addProducts")}
+          >
+            <SupplierIcon />
+            Fornecedor
+          </Button>
+        </ButtonsContainer>
+      </MainContainer>
+    </MuiThemeProvider>
+  );
 }
 
 export default LoginPage;
--- src/components/LoginPage/styled.js
+++ src/components/LoginPage/styled.js
@@ -1,5 +1,33 @@
 import styled from "styled-components";
 
+export const HeaderText = styled.h1`
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: black;
+`;
+
 export const MainContainer = styled.div`
-  border: 1px solid blue;
+  display: flex;
+  flex-direction: column;
+  width: 90vw;
+  margin: 0 auto;
+  justify-content: space-evenly;
+  align-items: center;
+  border: 1px solid green;
+  height: 80vh;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  margin: 0 auto;
+  width: 90%;
+  height: 10vh;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid purple;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
 `;

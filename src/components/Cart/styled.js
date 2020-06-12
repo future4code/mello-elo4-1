b4f506fd429d10e1bd016856ef838ac2a33edbb3
Diego <di__e__go@hotmail.com>
2020-06-12 13:09:44 -0300

    Mudança no estilo do carrinho com Material UI, e add botão finalizar

--- src/components/Cart/styled.js
+++ src/components/Cart/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 //import { Grid } from "@material-ui/core";
 
 // Aqui vamos mudar pro import { Grid } from '@material-ui/core';
+
 export const MainContainer = styled.div`
   border: 1px solid blue;
 `;
